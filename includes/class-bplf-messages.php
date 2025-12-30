8b16b377d537304e1df935369542feeb4e158b0d
kahnu044 <kahnucharan044@gmail.com>
2025-12-30 15:20:07 +0530

    Centralized plugin messages added

---
+++ includes/class-bplf-messages.php
@@ -0,0 +1,22 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Centralized plugin messages
+ */
+class BPLF_Messages {
+
+    public static function product_not_available() {
+        return apply_filters(
+            'bplf_product_not_available_message',
+            __( 'This product is not available in your location.', 'bplf' )
+        );
+    }
+
+    public static function cart_not_deliverable() {
+        return apply_filters(
+            'bplf_cart_not_deliverable_message',
+            __( 'Some items are not deliverable to your location.', 'bplf' )
+        );
+    }
+}
