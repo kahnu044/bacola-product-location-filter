59d9b6111dd2569b3b0c5e7521a6c12dcc136a65
kahnu044 <kahnucharan044@gmail.com>
2025-12-30 15:20:21 +0530

    Optimsed

--- includes/class-bplf-dependencies.php
+++ includes/class-bplf-dependencies.php
@@ -1,39 +1,43 @@
 <?php
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Handles dependency checks
  */
-class BPLF_Dependencies {
+class BPLF_Dependencies
+{
 
     /**
      * Check required theme & plugins
      */
-    public static function check() {
+    public static function check()
+    {
 
-        if ( ! class_exists( 'WooCommerce' ) ) {
-            add_action( 'admin_notices', [ __CLASS__, 'woocommerce_missing' ] );
+        if (! class_exists('WooCommerce')) {
+            add_action('admin_notices', [__CLASS__, 'woocommerce_missing']);
             return false;
         }
 
         $theme = wp_get_theme();
-        if ( stripos( $theme->get( 'Name' ), 'Bacola' ) === false ) {
-            add_action( 'admin_notices', [ __CLASS__, 'theme_missing' ] );
+        if (stripos($theme->get('Name'), 'Bacola') === false) {
+            add_action('admin_notices', [__CLASS__, 'theme_missing']);
             return false;
         }
 
         return true;
     }
 
-    public static function woocommerce_missing() {
+    public static function woocommerce_missing()
+    {
         echo '<div class="notice notice-error"><p>';
-        echo esc_html__( 'Bacola Location Product Control requires WooCommerce to be installed and active.', 'blpc' );
+        echo esc_html__('Bacola Location Product Control requires WooCommerce to be installed and active.', 'bplf');
         echo '</p></div>';
     }
 
-    public static function theme_missing() {
+    public static function theme_missing()
+    {
         echo '<div class="notice notice-warning"><p>';
-        echo esc_html__( 'This plugin is designed for the Bacola theme. Some features may not work properly.', 'blpc' );
+        echo esc_html__('This plugin is designed for the Bacola theme. Some features may not work properly.', 'bplf');
         echo '</p></div>';
     }
 }
