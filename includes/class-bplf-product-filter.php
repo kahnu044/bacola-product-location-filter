ff3384d40b5bb2aff10cbb712a4bf58209159ea9
kahnu044 <kahnucharan044@gmail.com>
2026-01-08 17:51:35 +0530

    Related Products Location Filter Added

--- includes/class-bplf-product-filter.php
+++ includes/class-bplf-product-filter.php
@@ -8,6 +8,7 @@ class BPLF_Product_Filter
     {
         add_action('pre_get_posts', [$this, 'filter_products']);
         add_action('template_redirect', [$this, 'block_single_product']);
+        add_filter('woocommerce_related_products', [$this, 'filter_related_products'], 10, 3);
     }
 
     public function filter_products($query)
@@ -41,6 +42,41 @@ class BPLF_Product_Filter
             wp_redirect(wc_get_page_permalink('shop'));
             exit;
         }
+    }
+
+    public function filter_related_products($related_posts, $product_id, $args)
+    {
+
+        // Get location from cookie
+        $location = BPLF_Helpers::get_user_location();
+
+        // If location is NOT set → return blank
+        if (empty($location)) {
+            return $related_posts;
+        }
+
+        $filtered_products = [];
+
+        foreach ($related_posts as $related_post_id) {
+
+            // Get location terms of related product
+            $terms = get_the_terms($related_post_id, 'location');
+
+            if (is_wp_error($terms) || empty($terms)) {
+                continue;
+            }
+
+            // Extract slugs
+            $slugs = wp_list_pluck($terms, 'slug');
+
+            // Match cookie location with product location
+            if (in_array($location, $slugs, true)) {
+                $filtered_products[] = $related_post_id;
+            }
+        }
+
+        // Return ONLY matched products (blank if none found)
+        return $filtered_products;
     }
 }
 
